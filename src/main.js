8b1bafb083ba1d00b2107c9109818803c9ac2b25
AerySun <janinequinten@gmail.com>
2021-07-21 11:40:13 +0200

    Map champ. ids to champ. names

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
+const { prompt } = require('enquirer')
+const { readFile } = require('fs/promises')
+const fetch = require('node-fetch')
+
 const API_KEY = 'RGAPI-11b9e6ec-ea77-4b99-9e6d-7b78674518ea'
 const BASE_URL = 'https://euw1.api.riotgames.com'
 const GET_SUMMONER_BY_NAME = 'lol/summoner/v4/summoners/by-name'
 const GET_LEAGUE_BY_SUMMONER = 'lol/league/v4/entries/by-summoner'
 const GET_MASTERY_BY_SUMMONER = '/lol/champion-mastery/v4/champion-masteries/by-summoner'
-const { prompt } = require('enquirer')
 
-const fetch = require('node-fetch')
-
+async function getChampions(){
+    const championsList = JSON.parse(await readFile('./src/champions.json'))
+    return Object.values(championsList.data).reduce((result, champion) => {
+        return {
+            ...result, 
+            [champion.key]: champion.name
+        }
+    }, {})
+}
 
 async function main() {
     const response = await prompt({
@@ -29,8 +39,16 @@ function getLeagueBySummonerId(id) {
     return sendRequest(GET_LEAGUE_BY_SUMMONER, id)
 }
 
-function getMasteryBySummonerId(id){
-    return sendRequest(GET_MASTERY_BY_SUMMONER, id)
+async function getMasteryBySummonerId(id){
+    const champions = await getChampions()
+    const response = await sendRequest(GET_MASTERY_BY_SUMMONER, id)
+    return response.map(mastery => {
+        return {
+            champion: champions[mastery.championId],
+            championLevel: mastery.championLevel,
+            championPoints: mastery.championPoints
+        }
+    }).filter(mastery => mastery.champion !== undefined)
 }
 
 async function sendRequest(path, param){
